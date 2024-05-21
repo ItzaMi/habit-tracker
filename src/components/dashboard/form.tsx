49a831292d9567cb8c714662009c6f1289046d82
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2024-05-21 23:17:57 +0100

    feat(dashboard): add tooltip to form

--- src/components/dashboard/form.tsx
+++ src/components/dashboard/form.tsx
@@ -2,6 +2,13 @@
 import { Plus } from 'lucide-react';
 import { FC, useRef } from 'react';
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+
 interface Props {
   action: (formData: FormData) => void;
 }
@@ -10,30 +17,54 @@ const Form: FC<Props> = ({ action }) => {
   const ref = useRef<HTMLFormElement>(null);
 
   return (
-    <form
-      className="flex flex-row items-center justify-between gap-1"
-      ref={ref}
-      action={(formData) => {
-        action(formData);
-        ref.current?.reset();
-      }}
-    >
-      <div className="flex h-10 w-full items-center rounded-lg border-2  border-slate-200 px-4 text-sm">
-        <input
-          id="habit"
-          type="text"
-          name="habit"
-          placeholder="Habit name"
-          className="w-full outline-none"
-        />
+    <div className="flex flex-col gap-2">
+      <div className="flex flex-row items-center gap-1">
+        <p className="text-xs font-bold">Start a new habit</p>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <button className="flex h-3 w-3 items-center justify-center rounded-full bg-black text-[8px] text-white">
+                ?
+              </button>
+            </TooltipTrigger>
+            <TooltipContent className="max-w-[200px] " side="right">
+              <p className="text-xs">
+                Think hard about what you want to achieve!
+                <br />
+                <strong>
+                  Once you create an habit and 48 hours pass, you no longer will
+                  be able to delete it.
+                </strong>
+              </p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
-      <button
-        type="submit"
-        className="bg-slate-white flex h-10 w-10 flex-shrink-0 cursor-pointer items-center justify-center rounded-lg border-2 border-slate-200 hover:border-slate-100"
+      <form
+        className="flex flex-row items-center justify-between gap-1"
+        ref={ref}
+        action={(formData) => {
+          action(formData);
+          ref.current?.reset();
+        }}
       >
-        <Plus size={12} />
-      </button>
-    </form>
+        <div className="flex h-10 w-full items-center rounded-lg border-2  border-slate-200 px-4 text-sm">
+          <input
+            id="habit"
+            type="text"
+            name="habit"
+            placeholder="Habit name"
+            className="w-full outline-none"
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-slate-white flex h-10 w-10 flex-shrink-0 cursor-pointer items-center justify-center rounded-lg border-2 border-slate-200 hover:border-slate-100"
+        >
+          <Plus size={12} />
+        </button>
+      </form>
+    </div>
   );
 };
 
